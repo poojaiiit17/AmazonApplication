1377d1c88c5a1fd2bcad84c9e4ed7f62a388c9eb
poojaiiit17 <poojaiiit17@gmail.com>
2026-08-16 15:46:01 +0530

    Complete cart service for customer shopping flow

--- src/main/java/com/amazon/service/CartService.java
+++ src/main/java/com/amazon/service/CartService.java
@@ -2,22 +2,31 @@ package com.amazon.service;
 
 import com.amazon.dao.CartDAO;
 import com.amazon.model.CartItem;
+import com.amazon.model.Product;
+
+import java.util.List;
 
 public class CartService {
 
-    private CartDAO cartDAO;
+    private final CartDAO cartDAO;
 
     public CartService() {
         cartDAO = new CartDAO();
     }
 
     public int createCart(int customerId) {
+        return cartDAO.getOrCreateCart(customerId);
+    }
 
-        return cartDAO.createCart(customerId);
+    public boolean addToCart(int customerId, int productId, int quantity) {
+        return cartDAO.addToCart(customerId, productId, quantity);
     }
 
     public boolean addToCart(CartItem item) {
-
         return cartDAO.addToCart(item);
+    }
+
+    public List<Product> getCartProducts(int customerId) {
+        return cartDAO.getCartProducts(customerId);
     }
 }

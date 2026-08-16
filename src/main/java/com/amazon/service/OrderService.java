d7c693dde6cd810fabf54612deb72a5f8f9cf2cd
poojaiiit17 <poojaiiit17@gmail.com>
2026-08-16 16:18:45 +0530

    Add order placement and order history service methods

--- src/main/java/com/amazon/service/OrderService.java
+++ src/main/java/com/amazon/service/OrderService.java
@@ -3,22 +3,31 @@ package com.amazon.service;
 import com.amazon.dao.OrderDAO;
 import com.amazon.model.Order;
 import com.amazon.model.OrderItem;
+import com.amazon.model.Product;
+
+import java.util.List;
 
 public class OrderService {
 
-    private OrderDAO orderDAO;
+    private final OrderDAO orderDAO;
 
     public OrderService() {
         orderDAO = new OrderDAO();
     }
 
     public int createOrder(Order order) {
-
         return orderDAO.createOrder(order);
     }
 
     public boolean addOrderItem(OrderItem item) {
-
         return orderDAO.addOrderItem(item);
+    }
+
+    public int placeOrder(Order order, List<Product> products) {
+        return orderDAO.placeOrder(order, products);
+    }
+
+    public List<Order> getOrdersByCustomer(int customerId) {
+        return orderDAO.getOrdersByCustomer(customerId);
     }
 }

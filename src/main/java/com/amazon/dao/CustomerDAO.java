b375637af356aca7af0e210caee14461c22f5a58
poojaiiit17 <poojaiiit17@gmail.com>
2026-08-16 15:28:20 +0530

    Add customer ID lookup for session login

--- src/main/java/com/amazon/dao/CustomerDAO.java
+++ src/main/java/com/amazon/dao/CustomerDAO.java
@@ -25,9 +25,33 @@ public class CustomerDAO {
             return rs.next();
 
         } catch (Exception e) {
-
             e.printStackTrace();
             return false;
         }
+    }
+
+    public int getCustomerId(String email, String password) {
+
+        String sql = "SELECT customer_id FROM customers WHERE email = ? AND password = ?";
+
+        try (
+                Connection con = DBConnection.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ) {
+
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("customer_id");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return -1;
     }
 }
